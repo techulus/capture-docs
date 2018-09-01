c24159bfd955b89e1f7cfdf4d89d43e332162188
Arjun Komath <arjunkomath@gmail.com>
2018-09-01 15:00:26 +0530

    Add google analytics

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -67,11 +67,13 @@ const siteConfig = {
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: 'default',
+    theme: 'github',
   },
 
   // Add custom scripts here that would be placed in <script> tags.
   scripts: ['https://buttons.github.io/buttons.js'],
+
+  gaTrackingId: 'UA-4655726-11',
 
   // On page navigation for the current documentation page.
   onPageNav: 'separate',
