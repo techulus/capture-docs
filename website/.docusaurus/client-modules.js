96eb6a1e60fe0f3f9dc6a735527e39c9fd387686
Arjun Komath <arjun@hey.com>
2022-11-13 12:50:41 +1100

    Handle landing page redirect

--- website/.docusaurus/client-modules.js
+++
@@ -1,5 +0,0 @@
-export default [
-  require('/Users/arjunkomath/Developer/Capture/capture-docs/web/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/arjunkomath/Developer/Capture/capture-docs/web/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/arjunkomath/Developer/Capture/capture-docs/web/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-];
--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,12 +1,6 @@
-import Layout from "@theme/Layout";
-import React from "react";
+import React from 'react';
+import {Redirect} from '@docusaurus/router';
 
-export default () => {
-  return <Layout title="Capture">
-    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
-
-      <h3>Display screenshots anywhere with the best screenshot automation tool<br />
-        Lightning fast PDF generation from URLs and HTML</h3>
-    </div>
-  </Layout>;
+export default function Home() {
+  return <Redirect to="/docs/get-started" />;
 };
